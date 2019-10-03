2397e281f6374e99e2da16fbf8520898f85c3156
jberry93 <jeffreyberry93@gmail.com>
2019-10-03 10:58:20 -0700

    chore(update): utilize ng-8.* files to rebuild and update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrayComponent } from './tray/tray.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        TrayComponent,
-    ],
-    imports: [
-        BrowserModule
-    ],
-    providers: [],
-    bootstrap: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    TrayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
